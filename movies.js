21a7db1859bf4281094fdbd9c3bb0171f342d3d9
momycani <momycani>
2025-12-08 18:28:46 -0800

    2nd save

--- movies.js
+++ movies.js
@@ -1,1 +1,76 @@
-//  http://www.omdbapi.com/?i=tt3896198&apikey=fca438ff
+//  https://www.omdbapi.com/?i=tt3896198&apikey=fca438ff&s=christmas
+//  https://www.omdbapi.com/?i=tt3896198&apikey=fca438ff&s=family
+//  https://www.omdbapi.com/?i=tt3896198&apikey=fca438ff&s=action
+
+const movies = [
+{
+"Title": "The Nightmare Before Christmas",
+"Year": "1993",
+"imdbID": "tt0107688",
+"Type": "movie",
+"Poster": "https://m.media-amazon.com/images/M/MV5BNmYxOTAzZWYtOGI3Yi00ODc3LTk5ZjYtZTY0MzVkZTg3YmRiXkEyXkFqcGc@._V1_SX300.jpg"
+},
+{
+"Title": "How the Grinch Stole Christmas",
+"Year": "2000",
+"imdbID": "tt0170016",
+"Type": "movie",
+"Poster": "https://m.media-amazon.com/images/M/MV5BNDAwMjFhYTEtNzYyMS00YmY2LTg3MGEtZTRkNWNiMDI4MDRkXkEyXkFqcGc@._V1_SX300.jpg"
+},
+{
+"Title": "National Lampoon's Christmas Vacation",
+"Year": "1989",
+"imdbID": "tt0097958",
+"Type": "movie",
+"Poster": "https://m.media-amazon.com/images/M/MV5BZDgxYzI2YWItNmUyNS00ZWE4LWEzZDctYTk1M2VkYjhkOWVmXkEyXkFqcGc@._V1_SX300.jpg"
+},
+{
+"Title": "A Christmas Story",
+"Year": "1983",
+"imdbID": "tt0085334",
+"Type": "movie",
+"Poster": "https://m.media-amazon.com/images/M/MV5BZWZhYzI1YjktYzJjYy00NjIwLWE0MGItNWM4YWM2ZjFkNGM0XkEyXkFqcGc@._V1_SX300.jpg"
+},
+{
+"Title": "A Christmas Carol",
+"Year": "2009",
+"imdbID": "tt1067106",
+"Type": "movie",
+"Poster": "https://m.media-amazon.com/images/M/MV5BMTM1MTI5ODU4MV5BMl5BanBnXkFtZTcwNTYyNTU4Mg@@._V1_SX300.jpg"
+},
+{
+"Title": "Last Christmas",
+"Year": "2019",
+"imdbID": "tt8623904",
+"Type": "movie",
+"Poster": "https://m.media-amazon.com/images/M/MV5BY2NlNTMwYzgtZjI2Ny00ZWExLWE2NDUtNzFlYTQyMmY1NjkwXkEyXkFqcGc@._V1_SX300.jpg"
+},
+{
+"Title": "Office Christmas Party",
+"Year": "2016",
+"imdbID": "tt1711525",
+"Type": "movie",
+"Poster": "https://m.media-amazon.com/images/M/MV5BMDhiMjg4ODMtYjgyNi00YjZkLWE0ZGItNjU0NGVlMzViZjdlXkEyXkFqcGc@._V1_SX300.jpg"
+},
+{
+"Title": "The Christmas Chronicles",
+"Year": "2018",
+"imdbID": "tt2990140",
+"Type": "movie",
+"Poster": "https://m.media-amazon.com/images/M/MV5BNTA3NjU3OTM2MV5BMl5BanBnXkFtZTgwNjQ2MzE1NjM@._V1_SX300.jpg"
+},
+{
+"Title": "The Muppet Christmas Carol",
+"Year": "1992",
+"imdbID": "tt0104940",
+"Type": "movie",
+"Poster": "https://m.media-amazon.com/images/M/MV5BNTIwOTAyMDgtNTY1Zi00ZWMyLWFjMjAtNjI5YWIyOTE2NDJhXkEyXkFqcGc@._V1_SX300.jpg"
+},
+{
+"Title": "A Very Harold & Kumar Christmas",
+"Year": "2011",
+"imdbID": "tt1268799",
+"Type": "movie",
+"Poster": "https://m.media-amazon.com/images/M/MV5BMzA1NTQzMjAyOV5BMl5BanBnXkFtZTcwNDcyMjY2Ng@@._V1_SX300.jpg"
+}
+];
